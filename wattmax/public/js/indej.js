344781411b581cec31850fe44877c0f6983c984d
williamnrqn <william.niarquin@epitech.eu>
2023-10-07 12:19:30 +0200

    modif error to prod

---
+++ wattmax/public/js/indej.js
@@ -0,0 +1,16 @@
+const listWrapperEl = document.querySelector('.side-scroll-list-wrapper');
+const listEl = document.querySelector('.side-scroll-list');
+
+gsap.to(listEl, {
+  x: () => -(listEl.clientWidth - listWrapperEl.clientWidth),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.side-scroll',
+    start: 'top top',
+    end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,
+    scrub: true,
+    pin: true,
+    anticipatePin: 1,
+    invalidateOnRefresh: true,
+  },
+});
